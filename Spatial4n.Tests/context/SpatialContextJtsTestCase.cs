3ae88f59410ca344701c2b5589b37bf9a14cd79f
Matt Warren <matt.warren@live.co.uk>
2012-04-30 13:11:37 +0100

    Completed work on the "context" tests, now the whole Java test suite is ported and all the tests pass.

---
+++ Spatial4n.Tests/context/SpatialContextJtsTestCase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spatial4n.Core.Context;
+using Spatial4n.Core.Distance;
+
+namespace Spatial4n.Tests.context
+{
+	//Don't think we need this, we're not porting JtsSpatialContext, so no need to test it
+
+	//class SpatialContextJtsTestCase : BaseSpatialContextTestCase
+	//{
+	//    protected override SpatialContext GetSpatialContext()
+	//    {
+	//        return new JtsSpatialContext(DistanceUnits.KILOMETERS);
+	//    }
+	//}
+}

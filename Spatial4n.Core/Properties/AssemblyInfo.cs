38e924d73dd03fe7f701455328f12748c5832f71
synhershko <itamar@code972.com>
2012-09-21 14:19:13 +0300

    Updating AssemblyInfo, marking v0.3

--- Spatial4n.Core/Properties/AssemblyInfo.cs
+++ Spatial4n.Core/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Spatial4n.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Geospatial Library for .NET - port of Spatial4j")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Spatial4n.Core")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("(c) spatial4j and Itamar Syn-Hershko")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]

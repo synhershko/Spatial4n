79db540e2633ba0a95a1176e8026d72ca4fceaad
Shad Storhaug <shad@shadstorhaug.com>
2016-11-22 02:08:36 +0700

    Changed VersionInfo to CommonAssemblyInfo, since we also take care of things such as copyright there

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("(c) spatial4j and Itamar Syn-Hershko")]
+[assembly: AssemblyInformationalVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
+
+
+

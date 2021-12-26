0bceb3aeceac2e820fc3c32a67c83123e461f49e
Shad Storhaug <shad@shadstorhaug.com>
2021-12-27 04:23:46 +0700

    Generate InternalsVisibleTo using MSBuild to make it possible to toggle assembly signing without breaking visibility

--- Spatial4n.Core.NTS/Properties/AssemblyInfo.cs
+++ Spatial4n.Core.NTS/Properties/AssemblyInfo.cs
@@ -18,11 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("efd57d2c-a197-4030-87ed-781ed35e4dc4")]
-
-// NOTE: AssemblyFileVersion and AssemblyInformationalVersion attributes are managed
-// by the build process in the common VersionInfo.cs file. Because we are strong-named,
-// we cannot update the AssemblyVersion without making a binary-incompatible assembly.
-// However, these dirty details are normally hidden behind NuGet, which uninstalls the old
-// assembly and reinstalls the new one. We supply the ACTUAL version number (with a build segment) as the AssemblyFileVersion
-// and, if there is any pre-release info, it is supplied as the AssemblyInformationalVersion, which is
-// the only built-in .NET attribute which allows us to provide a custom version format.

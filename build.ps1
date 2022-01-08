8b2b6226379cd68c873506ee065834f5f956901e
Shad Storhaug <shad@shadstorhaug.com>
2022-01-09 06:45:36 +0700

    build.ps1: Fixed whitespace and added aliases for mulit-word parameter names

--- build.ps1
+++ build.ps1
@@ -1,55 +1,54 @@
 # Parses and validates the command arguments and bootstraps the Psake build script with the cleaned values
 
 function Get-NextArg([string[]]$arguments, [int]$i, [string]$argName) {
-	$i++
-	if ($arguments.Length -gt $i -and -not $($arguments[$i]).StartsWith('-')) {
-		return $arguments[$i]
-	} else {
-		throw $("'$argName' requires a value to be passed as the next argument")
-	}
+    $i++
+    if ($arguments.Length -gt $i -and -not $($arguments[$i]).StartsWith('-')) {
+        return $arguments[$i]
+    } else {
+        throw $("'$argName' requires a value to be passed as the next argument")
+    }
 }
 
-[string]$packageVersion = ''
 [string]$packageVersion = ''
 [string]$fileVersion = ''
 [string]$configuration = 'Release'
 [bool]$runTests = $false
 
 for ([int]$i = 0; $i -lt $args.Length; $i++) {
-	$arg = $args[$i]
-	$loweredArg =  "$arg".ToLowerInvariant()
-	
-	if ($loweredArg -eq '-t' -or $loweredArg -eq '--test') {
-		$runTests = $true
-	} elseif ($loweredArg -eq '-pv' -or $loweredArg -eq '--packageversion') {
-		$packageVersion = Get-NextArg $args $i $arg
-		$i++
-	} elseif ($loweredArg -eq '-v' -or $loweredArg -eq '--version') {
-		$fileVersion = Get-NextArg $args $i $arg
-		$i++
-	} elseif ($loweredArg -eq '-config' -or $loweredArg -eq '--configuration') {
-		$configuration = Get-NextArg $args $i $arg
-		$i++
-	} else {
-		throw $("Unrecognized argument: '$arg'")
-	}
+    $arg = $args[$i]
+    $loweredArg =  "$arg".ToLowerInvariant()
+    
+    if ($loweredArg -eq '-t' -or $loweredArg -eq '--test') {
+        $runTests = $true
+    } elseif ($loweredArg -eq '-pv' -or $loweredArg -eq '--package-version' -or $loweredArg -eq '--packageversion') {
+        $packageVersion = Get-NextArg $args $i $arg
+        $i++
+    } elseif ($loweredArg -eq '-fv' -or $loweredArg -eq '--file-version' -or $loweredArg -eq '--fileversion') {
+        $fileVersion = Get-NextArg $args $i $arg
+        $i++
+    } elseif ($loweredArg -eq '-config' -or $loweredArg -eq '--configuration') {
+        $configuration = Get-NextArg $args $i $arg
+        $i++
+    } else {
+        throw $("Unrecognized argument: '$arg'")
+    }
 }
 
 [string[]]$task = 'Pack'
 if ($runTests) {
-	$task = 'Pack','Test'
+    $task = 'Pack','Test'
 }
 $parameters = @{}
 $properties = @{}
 
 if (-not [string]::IsNullOrWhiteSpace($packageVersion)) {
-	$properties.packageVersion=$packageVersion
+    $properties.packageVersion=$packageVersion
 }
 if (-not [string]::IsNullOrWhiteSpace($fileVersion)) {
-	$properties.fileVersion=$fileVersion
+    $properties.fileVersion=$fileVersion
 }
 if (-not [string]::IsNullOrWhiteSpace($configuration)) {
-	$properties.configuration=$configuration
+    $properties.configuration=$configuration
 }
 
 Import-Module "$PSScriptRoot/.build/psake/psake.psm1"
